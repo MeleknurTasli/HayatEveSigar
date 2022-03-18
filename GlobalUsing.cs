5b85d5180785aa4772ddb36b70ae7cd6b0ad51a2
yakupkamci <yakup.kamci@gmail.com>
2022-03-18 16:52:32 +0300

    file system

---
+++ GlobalUsing.cs
@@ -0,0 +1,13 @@
+global using System;
+global using System.Net;
+global using System.Text;
+global using System.Text.Json.Serialization;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Filters;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using FluentValidation;
+global using FluentValidation.AspNetCore;
